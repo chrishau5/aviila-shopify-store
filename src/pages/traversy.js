7e8023ea2c6e295a3101ecc6a4a8a8b83036d75c
christophish91 <31976652+christophish91@users.noreply.github.com>
2021-07-16 02:44:53 -0400

    Media Query set for Traversy

--- src/pages/traversy.js
+++ src/pages/traversy.js
@@ -1,8 +1,18 @@
-import * as React from "react"
+import React, { useState } from "react"
 import TestVid from "../videos/miami.mp4"
 import "@fontsource/montserrat"
 import "../css/typography.css"
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
+
+const COLORS = {
+  colorText: "#f6ebeb",
+  colorBackground: "rgba(12, 12, 14, 0.4)",
+  colorLinkHover: "#ddfc74",
+}
+
+const MEDIA = {
+  test: "798px",
+}
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -11,7 +21,7 @@ const Wrapper = styled.div`
   font-family: "Montserrat";
 `
 
-const Showcase = styled.div`
+const Showcase = styled.section`
   position: absolute;
   right: 0;
   width: 100%;
@@ -22,10 +32,17 @@ const Showcase = styled.div`
   align-items: center;
   background: #111;
   color: #fff;
-  z-index;
+  z-index: 2;
+  &.active {
+    right: 300px;
+  }
+
+  @media (max-width: ${MEDIA.test}) {
+    padding: 40px;
+  } ;
 `
 
-const Header = styled.div`
+const Header = styled.header`
   position: absolute;
   top: 0;
   left: 0;
@@ -35,12 +52,17 @@ const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: ${MEDIA.test}) {
+    padding: 40px;
+  } ;
 `
 
 const Logo = styled.h2`
   font-family: Ailerons;
   font-size: 1.44em;
   font-weight: 600;
+  cursor: pointer;
 `
 
 const Toggle = styled.div`
@@ -68,6 +90,7 @@ const Video = styled.video`
   height: 100%;
   object-fit: cover;
   opacity: 0.8;
+  will-change: opacity;
 `
 
 const Overlay = styled.div`
@@ -78,7 +101,7 @@ const Overlay = styled.div`
   height: 100%;
   background: rgba(12, 12, 14, 0.4);
   will-change: opacity;
-  mix-blend-mode: overlay;
+  /* mix-blend-mode: overlay; */
 `
 
 const Text = styled.div`
@@ -93,6 +116,10 @@ const Title = styled.div`
   text-transform: uppercase;
   font-family: Ailerons;
   letter-spacing: -0.05em;
+
+  @media (max-width: ${MEDIA.test}) {
+    font-size: 3em;
+  } ;
 `
 
 const Subtitle = styled.div`
@@ -102,16 +129,110 @@ const Subtitle = styled.div`
   text-transform: uppercase;
   font-family: Ailerons;
   letter-spacing: -0.07em;
+
+  @media (max-width: ${MEDIA.test}) {
+    font-size: 2em;
+  } ;
 `
 
 const Paragraph = styled.div`
   margin: 20px 0;
   font-weight: 500;
+  font-size: 1.1em;
+  max-width: 700px;
 `
 
-const Menu = styled.div``
+const ExploreButton = styled.div`
+  display: inline-block;
+  font-size: 1em;
+  background: white;
+  padding: 10px 30px;
+  text-decoration: none;
+  color: black;
+  margin-top: 10px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  transition: 0.2s;
+  &:hover {
+    letter-spacing: 6px;
+  }
+`
 
-export default function Traversy() {
+const Social = styled.ul`
+  position: absolute;
+  bottom: 30px;
+  z-index: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & li {
+    list-style: none;
+    & a {
+      display: inline-block;
+      filter: invert(1);
+      margin-right: 50px;
+      transform: scale(1.2);
+      transition: 0.5s;
+      &:hover {
+        transform: scale(1.2) translateY(-15px);
+      }
+    }
+  }
+`
+
+const Menu = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  /* right: 1%; */
+  width: 300px;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background: rgba(12, 12, 14, 1);
+  /* backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18); */
+
+  & ul {
+    position: relative;
+    & li {
+      list-style: none;
+      padding-right: 52.36px;
+      text-align: right;
+      margin-bottom: 20px;
+      & a {
+        text-decoration: none;
+        font-size: 24px;
+        font-weight: 600;
+        letter-spacing: 2px;
+        color: ${COLORS.colorText};
+        padding: 1px;
+        border-bottom: 1px solid ${COLORS.colorText};
+
+        &: hover {
+          color: ${COLORS.colorLinkHover};
+          border-bottom: 3px solid ${COLORS.colorLinkHover};
+          font-weight: 900;
+        }
+      }
+    }
+  }
+`
+
+// const menuToggle = document.querySelector(".toggle")
+// const showcase = document.querySelector(".showcase")
+
+// menuToggle.addEventListener("click", () => {
+//   menuToggle.classList.toggle("active")
+//   showcase.classList.toggle("active")
+// })
+
+export default function Traversy({ children }) {
+  // const [open, setOpen] = useState(false);
+
   return (
     <Wrapper>
       <Showcase>
@@ -130,19 +251,25 @@ export default function Traversy() {
             magna deserunt elit sunt. Deserunt adipisicing enim reprehenderit
             veniam amet qui officia cupidatat sit Lorem non nulla duis.
           </Paragraph>
-          <a href="#">Explore</a>
+          <ExploreButton href="#">Explore</ExploreButton>
         </Text>
-        <ul className="social">
+        <Social className="social">
           <li>
-            <img src="https://i.ibb.co/ySwtH4B/instagram.png" alt="" />
+            <a href="https://www.instagram.com/aviila_apparel/">
+              <img src="https://i.ibb.co/ySwtH4B/instagram.png" alt="" />
+            </a>
           </li>
           <li>
-            <img src="https://i.ibb.co/x7P24fL/facebook.png" alt="" />
+            <a href="https://www.facebook.com/AviilaApparel/">
+              <img src="https://i.ibb.co/x7P24fL/facebook.png" alt="" />
+            </a>
           </li>
           <li>
-            <img src="https://i.ibb.co/Wnxq2Nq/twitter.png" alt="" />
+            <a href="https://twitter.com/aviila_apparel">
+              <img src="https://i.ibb.co/Wnxq2Nq/twitter.png" alt="" />
+            </a>
           </li>
-        </ul>
+        </Social>
       </Showcase>
       <Menu>
         <ul>
